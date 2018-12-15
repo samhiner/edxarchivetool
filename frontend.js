29d1f8d69fa099ef7da3cc0d52323c337b3c7fa0
samhiner <25464398+samhiner@users.noreply.github.com>
2018-12-15 11:54:54 -0500

    Answer checking for singular questions fully working.

--- frontend.js
+++ frontend.js
@@ -1,23 +1,23 @@
-PASSWORD = 'INSERT_A_PASSWORD';
+//TODO make it so the script can handle a bad password
+
+PASSWORD = 'jellyfish';
 
 //setup
 var buttons = document.getElementsByClassName('submit btn-brand');
 for (var x = 0; x < buttons.length; x++) {
 	buttons[x].disabled = false;
 	updateFormat(buttons[x]);
+
+	//make button
 	buttons[x].onclick = function(event) {
 		check(event);
 	};
-
-	//if button is supposed to stay on keep it on
-	if (buttons[x].disabled == false) {
-		keepSubmit(buttons[x]);
-	}
 }
 
 //make sure that the submit does not get greyed out onclick
 //for some reason it happens with questions with multiple responses
-function keepSubmit(element) {
+//"keep" is a bool that can reverse function to destroy onclick for the responseArea
+function keepSubmit(element, keep = true) {
 	var responses = element.parentElement.parentElement.parentElement.children[0].children;
 	//sometimes the questions are stored inside an extra span
 	if (responses.length === 1) {
@@ -26,24 +26,24 @@ function keepSubmit(element) {
 
 	var responseArea = element.parentElement.parentElement.parentElement.children[0];
 
-	responseArea.onclick = function(event) {
-		element.disabled = false;
-	};
+	if (keep) {
+		responseArea.onclick = function(event) {
+			element.disabled = false;
+			console.log('click-on')
+		};
+	} else {
+		responseArea.onclick = function() { console.log('click-off') };
+	}
 }
 
-//get info about a question that is needed for lcoating it in the sheet
+//get info about a question that is needed for locating it in the sheet
 function getQuestionInfo(element) {
 	var lesson = document.title.split(' ')[0];
 	var problem = parseInt(getLastNum(element.parentElement.parentElement.parentElement.previousElementSibling.previousElementSibling.firstChild.data));
 	var possPoints = parseInt(getLastNum(element.parentElement.parentElement.parentElement.previousElementSibling.firstChild.data));
 	var possTries = parseInt(getLastNum(element.nextElementSibling.childNodes[0].data));
 
-	console.log(lesson);
-	console.log(problem);
-	console.log(possPoints);
-	console.log(possTries);
-
-	return lesson, problem, possPoints, possTries;
+	return [lesson, problem, possPoints, possTries];
 }
 
 function getLastNum(string) {
@@ -56,47 +56,66 @@ function getLastNum(string) {
 	}
 }
 
+//THIS IS REALLY BROKEN because I can't figure out how to get it to not have a cross-origin error or execute the result.
 //make question reflect the "tries" and "score" data in the spreadsheet
 function updateFormat(element) {
-	var lesson, problem, possPoints, possTries = getQuestionInfo(element);
-	var args = 'lesson:' + lesson + ',problem:' + problem + ',type:getTries,possTries:' + possTries + ',possPoints:' + possPoints + '&password=' + window.PASSWORD;
+	var questionInfo = getQuestionInfo(element);
+	var args = 'lesson:' + questionInfo[0] + ',problem:' + questionInfo[1] + ',type:getTries,possTries:' + questionInfo[3] + ',possPoints:' + questionInfo[2] + '&password=' + window.PASSWORD;
 
 	$.ajax({
 		url: 'https://script.google.com/macros/s/AKfycbyPdkk_VoJjlOolZChvmekYU70SHgDJy73Vn8PBDHR5Zl-UvuV9/exec?method=handleRequest&request=' + args,
-		dataType: 'jsonp',
+		datatype: 'jsonp',
+		callback: function() { console.log('hello') },
 		success: function(data) {
 			[score, tries] = data.split(',');
-			if (tries == possTries || score == possPoints) {
+			//if you have maxed out tries or score
+			if (tries == questionInfo[3] || score == questionInfo[2]) {
 				console.log('WOAHHHHHHHH')
 				element.disabled = true;
-				element.onclick = '';
+				keepSubmit(element, false);
 				//TODO add tries and score info
+			} else {
+				console.log('LLLLLLL')
+				//if button is supposed to stay on keep it on
+				keepSubmit(element);
 			}
-		}
+		},
 	});
 }
 
 //when submit is clicked, this compares your answer to the correct answer and runs submit().
 function check(event) {
 	//find the code for the answer URL
-	var problemCode = event.path[5].id.split('_')[1];
-	//sometimes the code can't be found for whatever reason
-	//so I just reload in those cases as this isn't production grade
-	if (problemCode === undefined) {
-		alert("Could not find this question's ID. Refreshing now.");
-		location.reload();
+	var problemCode = event.srcElement.parentElement.parentElement.parentElement.parentElement;
+
+	if (problemCode.id === '') {
+		problemCode = problemCode.parentElement.id;
+		var path = 1;
+	} else {
+		problemCode = problemCode.id;
+		var path = 2;
+		console.log('GO');
 	}
+
+	problemCode = problemCode.split('_')[1];
 
 	//get the answer
 	$.get('https://courses.edx.org/courses/course-v1:RiceX+AdvCAL.1x+2015_T3/xblock/block-v1:RiceX+AdvCAL.1x+2015_T3+type@problem+block@' + problemCode + '/handler/xmodule_handler/problem_show',	
 	function(data) {
-		choices = getChoices(event.srcElement.parentElement);
+		var choices = getChoices(event.srcElement.parentElement, path);
 
-		console.log(data.answers)
+		console.log(choices);
+		console.log(data.answers);
 
 		var x = 0;
 		var score = 0;
-		for (answer in data.answers) {
+		for (var answerIndex in data.answers) {
+			var answer = data.answers[answerIndex];
+
+			if (typeof answer === 'object') {
+				answer = answer[0];
+			}
+
 			if (choices[x] == answer) {
 				score += 1;
 			}
@@ -109,13 +128,20 @@ function check(event) {
 	});
 }
 
-function getChoices(element) {
-	var questions = element.parentElement.parentElement.parentElement.children[0].children;
-	//sometimes the questions are stored inside an extra span
-	if (questions.length === 1) {
-		questions = questions[0].children
+//path: there are two paths, 1 and 2 and the one you use depends on how
+//the html happened to be organized on this load. Pretty sure it loads
+//differently because the DOM is trying to process a bug but that is just
+//conjecture
+function getChoices(element, path) {
+	console.log(element.attributes)
+	var questions = element.parentElement.parentElement.parentElement.children
+
+	if (path === 2) {
+		questions = questions[2].children[0].children[0].children;
+	} else if (path === 1) {
+		questions = questions[0].children[0].children;
 	}
-	
+
 	var choices = [];
 	for (var x = 0; x < questions.length; x++) {
 		if (questions[x].nodeName === 'DIV') {
@@ -137,22 +163,21 @@ function getChoices(element) {
 		}
 	}
 
-	console.log(choices);
 	return choices;
 }
 
 //submits the new data to the google sheet that keeps track of progress
 function submit(score, element) {
 	//get the arguments for the URL
-	var lesson, problem, possPoints, possTries = getQuestionInfo(element);
-	var args = 'lesson:' + lesson + ',problem:' + problem + ',type:submit,possTries:' + possTries + ',possPoints:' + possPoints + ',score:' + score + '&password=' + window.PASSWORD;
+	var questionInfo = getQuestionInfo(element);
+	var args = 'lesson:' + questionInfo[0] + ',problem:' + questionInfo[1] + ',type:submit,possTries:' + questionInfo[3] + ',possPoints:' + questionInfo[2] + ',score:' + score + '&password=' + window.PASSWORD;
 
 	//submit the data
 	$.ajax({
 		url: 'https://script.google.com/macros/s/AKfycbyPdkk_VoJjlOolZChvmekYU70SHgDJy73Vn8PBDHR5Zl-UvuV9/exec?method=handleRequest&request=' + args,
 		//TODO do i need dataType: 'jsonp',
 		success: function(data) {
-			if (data[0] == 'E') {
+			if (data[0] === 'E') {
 				alert('Upload failed with "' + data + '"');
 			}
 		}
